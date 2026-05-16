1cf7145f178bf7cf3fe822feec81a3f68cc7fd3b
helloo-cmyk <helloo-cmyk@users.noreply.github.com>
2026-05-16 10:26:12 -0700

    fix: further refine CSP with scripts.clarity.ms and script-src-elem

--- ibrat-next/next.config.ts
+++ ibrat-next/next.config.ts
@@ -29,7 +29,8 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://clarity.ms https://www.clarity.ms https://pagead2.googlesyndication.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://clarity.ms https://www.clarity.ms https://scripts.clarity.ms https://pagead2.googlesyndication.com",
+      "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://clarity.ms https://www.clarity.ms https://scripts.clarity.ms https://pagead2.googlesyndication.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https:",
